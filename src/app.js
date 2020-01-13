d04a63662381c4b4ff67b5ab621f2e4c0d5f0055
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-01-13 15:46:03 +0000

    basic add functionality done

--- src/app.js
+++ src/app.js
@@ -4,17 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      todos: ["Buy shopping", "Clean bathroom", "Car's MOT"],
+      items: ["Buy shopping", "Clean bathroom", "Car's MOT"],
       newItem: ""
     },
     methods: {
-      // saveNewItem: function() {
-      //   this.items.push(this.newItem)
-      //   this.newItem = {name: "", isPurchased: false}
-      // },
-      // purchaseItem: function(indexPosition) {
-      //   this.items[indexPosition].isPurchased = true
-      // }
+      saveNewItem: function() {
+        this.items.push(this.newItem)
+      }
     }
   });
 });
